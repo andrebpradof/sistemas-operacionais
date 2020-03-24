91c8683e92c4891c44350b1086ea57a0572e5380
mateusdoimo <mateusfernandesdoimo@gmail.com>
2020-03-24 18:41:57 -0300

    arquivo i-o atualizado

--- parte1/e-s-arquivo-2.c
+++ parte1/e-s-arquivo-2.c
@@ -2,25 +2,32 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]){
+
     FILE *pf;
-    char texto[] = "Teste exemplo aqui!";
-    char pilido[255];
-    if ((pf = fopen("arquivo.txt", "wb")) == NULL){
+    char leitura[200];
+    char texto_user[200];
+
+    printf("Digite um texto de 200 caracteres: ");
+    scanf("%[^\n]s", texto_user);
+    setbuf(stdin, NULL);
+    printf("\n");
+    
+    if ((pf = fopen("arquivo.txt", "w")) == NULL){
         printf("Erro na abertura do arquivo");
         exit(1);
     }
-    if (fwrite(&texto,255, 1, pf) != 1) {
+    if (fwrite(&texto_user,200, 1, pf) != 1) {
         printf("Erro na escrita do arquivo");
     }
     fclose(pf);                                    /* Fecha o arquivo */
-    if ((pf = fopen("arquivo.txt", "rb")) == NULL){
+    if ((pf = fopen("arquivo.txt", "a+")) == NULL){
         printf("Erro na abertura do arquivo");
         exit(1);
     }
-    if (fread(&pilido,255, 1, pf) != 1) {
+    if (fread(&leitura,200, 1, pf) != 1) {
         printf("Erro na leitura do arquivo");
     }  
-    printf("\nO valor de PI, lido do arquivo e': %s \n", pilido);
+    printf("Texto do arquivo: %s \n", leitura);
     fclose(pf);
     return 0;
 }
