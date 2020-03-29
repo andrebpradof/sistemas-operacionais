01060967952a6d401b3bfa036f1dfb9ca8b80d74
andrebpradof <andrebpf@usp.br>
2020-03-29 15:15:14 -0300

    Co-authored-by: Mateus Fernandes Doimo <mateusdoimo@usp.br> Co-authored-by: diegoparrajau@hotmail.com <diegoparrajau@hotmail.com>

--- parte2/cpu-bound.c
+++ parte2/cpu-bound.c
@@ -1,19 +1,24 @@
+//////////////////////////////////////////////////////////////
+//  SSC0640 - Sistemas Operacionais I (2020)                //
+//  Exemplo: CPU-bound                                      //
+//                                                          //
+//  Alunos:                                                 //
+//  André Baconcelo Prado Furlanetti - Nº USP: 10748305     //
+//  Diego da Silva Parra             - Nº USP: 10716550     //
+//  Mateus Fernandes Doimo           - Nº USP: 10691971     //
+//////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
-
 #define TAM 100000
 
-void bubble_sort(int list[], int n)
-{
+//Algoritmo de ordenacao
+void bubble_sort(int list[], int n){
     int c, d, t;
 
-    for (c = 0; c < n - 1; c++)
-    {
-        for (d = 0; d < n - c - 1; d++)
-        {
-            if (list[d] > list[d + 1])
-            {
-                /* Swapping */
+    for (c = 0; c < n - 1; c++){
+        for (d = 0; d < n - c - 1; d++){
+            if (list[d] > list[d + 1]){
                 t = list[d];
                 list[d] = list[d + 1];
                 list[d + 1] = t;
@@ -22,17 +27,15 @@ void bubble_sort(int list[], int n)
     }
 }
 
-int main(int argc, char const *argv[])
-{
-    int vet[TAM];
+int main(int argc, char const *argv[]){
+    int *vet = calloc(TAM,sizeof(int)); //Alocacao de momoria
 
-    for (int i = 0; i < TAM; i++)
-    {
+    for (int i = 0; i < TAM; i++){
         /* gerando valores aleatórios entre zero e 1000 */
         vet[i] = rand() % 1000;
     }
 
-    bubble_sort(vet, TAM);
+    bubble_sort(vet, TAM); //Algoritimo de ordenacao dos valores do vetor
 
     return 0;
 }
--- parte2/i-o-bound.c
+++ parte2/i-o-bound.c
@@ -1,9 +1,13 @@
-/*
-Para compilar, rode: gcc iofile.c -Wall -o iofile
-O N indicado a seguir é 0, se é para rodar apenas 1 vez, ou 1, se é para rodar 10000 vezes
-Para executar com o arquivo padrão, rode: ./iofile N
-Para executar com um arquivo em especifico, rode: ./iofile N path/to/file
-*/
+//////////////////////////////////////////////////////////////
+//  SSC0640 - Sistemas Operacionais I (2020)                //
+//  Exemplo: I-O-bound                                      //
+//                                                          //
+//  Alunos:                                                 //
+//  André Baconcelo Prado Furlanetti - Nº USP: 10748305     //
+//  Diego da Silva Parra             - Nº USP: 10716550     //
+//  Mateus Fernandes Doimo           - Nº USP: 10691971     //
+//////////////////////////////////////////////////////////////
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -32,17 +36,15 @@ int main(int argc, char const *argv[])
 
     printf("Arquivo para ser copiado aberto com sucesso\n");
 
-    receiver = open("output.txt", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+    //receiver = open("output.txt", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 
     while(times > 0){
-        /*
-        Syscall read usada para leitura do arquivo, tendo como argumentos o 'file descriptor' do arquivo,
-        o buffer que receberá os caracteres do arquivo, e quantos caracteres devem ser lidos
-        Retorna o número de caracteres lidos ou -1 caso tenha ocorrido algum erro.
-        */
+
+        //lseek(file, 0, SEEK_SET);
+
         int check = read(file, buffer, B_SIZE);
 
-        if(check < 0){          //Verificação de erro da leitura do arquivo
+        if(check < 0){ //Verificação de erro da leitura do arquivo
             
             perror("file");
             exit(1);
@@ -53,7 +55,7 @@ int main(int argc, char const *argv[])
         
         times--;
 
-        write(receiver, buffer, check);
+        //write(receiver, buffer, check);
     }
 
     /*if(write(STDOUT_FILENO, buffer, B_SIZE) < 0){   //Verificação de erro da escrita ao STDOUT
@@ -61,7 +63,7 @@ int main(int argc, char const *argv[])
         perror("stdout: ");
         exit(1);
 
-    }//
+    }*/
 
     /*
     Syscall close utilizada para fechar o arquivo, tem como parametros o 'file descriptor' do arquivo
